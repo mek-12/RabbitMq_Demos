47ebcc13d9d005fb173d1991d553e922e80748ba
Muhammet Emin Karaboğa <mek.krbg04@gmail.com>
2024-07-27 20:06:25 +0300

    first push

---
+++ rabbitmqhelloworld/consumer/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+factory.UserName = "guest";
+factory.Password= "guest";
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = System.Text.Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+    Console.ReadKey();
+};
+channel.BasicConsume(queue: "hello",
+                     autoAck: true,
+                     consumer: consumer);
